3abfc9fe98a2dd849a3d375ef33d95509d483af6
Kayla Hennig <kayla.hennig@gmail.com>
2018-01-04 14:02:18 +0000

    added queue constructor

---
+++ queues.js
@@ -0,0 +1,47 @@
+class Queue {
+
+    constructor() {
+        this._oldestIndex = 1;
+        this._newestIndex = 1;
+        this._storage = {};
+    }
+
+    size() {
+        return this._newestIndex - this._oldestIndex;
+    }
+
+    enqueue(data) {
+        this._storage[this._newestIndex] = data;
+        this._newestIndex++;
+    }
+
+    dequeue() {
+        if (this._oldestIndex !== this._newestIndex) {
+
+            let deletedData = this._storage[this._oldestIndex];
+            delete this._storage[this._oldestIndex];
+            this._oldestIndex++;
+
+            return deletedData;
+        }
+    }
+
+    print() {
+        console.log(this._storage);
+    }
+}
+
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.print(); // => { '1': 1, '2': 2, '3': 3 }
+console.log('size is 3:', queue.size()); // => 3
+console.log('dequeue is 1:', queue.dequeue()); // => 1
+queue.print(); // => { '2': 2, '3': 3 }
+console.log('dequeue is 2:', queue.dequeue()); // => 2
+console.log('size is 1:', queue.size()); // => 1
+console.log('dequeue is 3:', queue.dequeue()); // => 3
+queue.print(); // => '{}'
+console.log('dequeue is undefined:', queue.dequeue()); // => undefined
--- stacks.js
+++ stacks.js
@@ -21,7 +21,7 @@ class Stack {
 		}
 	}
 	
-	length() {
+	size() {
 		return this._size;
 	}
 
@@ -35,12 +35,12 @@ const stack = new Stack();
 stack.push(1);
 stack.push(2);
 stack.push(3);
-stack.print(); // => 1 2 3
-console.log('length is 3:', stack.length()); // => 3
+stack.print(); // => { '1': 1, '2': 2, '3': 3 }
+console.log('size is 3:', stack.size()); // => 3
 console.log('pop is 3:', stack.pop()); // => 3
-stack.print(); // => 1 2
+stack.print(); // => { '1': 1, '2': 2 }
 console.log('pop is 2:', stack.pop()); // => 2
-console.log('length is 1:', stack.length()); // => 1
+console.log('size is 1:', stack.size()); // => 1
 console.log('pop is 1:', stack.pop()); // => 1
-stack.print(); // => ''
+stack.print(); // => '{}'
 console.log('pop is undefined:', stack.pop()); // => undefined
