f9aac1e3627dbf78fe1dd5b1c0f6c606b2acd611
hennigk <kayla.hennig@gmail.com>
2018-01-10 22:01:04 +0000

    refactored priority queue

--- priority-queue.js
+++ priority-queue.js
@@ -1,4 +1,7 @@
 /* 
+This implmentation uses bitwise operators
+if you are not familiar with bitwise operators
+refer to: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators#Right_shift
 
 assumptions:
 smaller number = highest priority
@@ -21,142 +24,164 @@ data
     5           10
 6       11  20      ___            
 array = [ 1, 5, 10, 6, 11, 20, __ ]
-pos   = [ 1, 2, 3 , 4, 5 , 6,  7  ]
+pos   = [ 0, 1, 2 , 3, 4 , 5,  6  ]
 */
+
+class Element {
+    constructor(priority, val) {
+        this.value = val;
+        this.priority = priority;
+    }
+}
+
 
 class PriorityQueue {
 
     constructor() {
-        this.queue = [null];
+        this.queue = [];
     }
 
-    insert(priority) {
-        const data = priority;
+    enqueue(value, priority) {
+        const newElement = new Element(value, priority);
+        this.queue.push(newElement);
+        this._bubbleUp(this.queue.length - 1);
+    }
 
-        //add data to queue at last position
-        this.queue.push(priority);
+    dequeue() {
+        const result = this.queue[0];
+        const lastValue = this.queue.pop();
 
-        //if queue only has one piece of data - return 
-        if (this.queue.length < 3) return;
+        if (this.queue.length > 0) {
+            this.queue[0] = lastValue;
 
-        //compare - always compare with a >> b
-        let currentPosition = this.queue.length - 1;
-        let parentPosition = currentPosition >> 1;
-        //   console.log('current: ' + currentPosition, 'parentPos: ' + parentPosition)
-        //   console.log('data: ' + this.queue[currentPosition] , 'parentVal: '+this.queue[parentPosition])
-
-        while (this.queue[currentPosition] < this.queue[parentPosition]) {
-            let pointer = this.queue[parentPosition];
-            //swap values
-            this.queue[parentPosition] = this.queue[currentPosition];
-            this.queue[currentPosition] = pointer;
-
-            currentPosition = parentPosition;
-            parentPosition = currentPosition >> 1;
+            this._bubbleDown(0);
         }
 
-
-        // console.log(this.queue);
+        return result;
     }
 
-    remove() {
+    _bubbleUp(position) {
 
-        if (this.queue.length > 1) {
+        let element = this.queue[position];
 
-            const toRemove = this.queue.slice(1, 2);
-            const lastValue = this.queue.pop();
+        // when at 0, an element is already at first position in array
+        while (position > 0) {
 
-            if (this.queue.length > 1) {
-                //remove last value of array
+            let parentPosition = position >> 1;
+            let parent = this.queue[parentPosition];
 
-
-                let currentPosition = 1;
-                let rightBranch = this.queue[currentPosition * 2];
-                let leftBranch = this.queue[currentPosition * 2 + 1];
-                //move last value to position one
-                this.queue[currentPosition] = lastValue;
-
-                while (this.queue[currentPosition] > rightBranch || this.queue[currentPosition] > leftBranch) {
-                    let comparePosition = rightBranch <= leftBranch ? currentPosition * 2 : currentPosition * 2 + 1;
-                    let pointer = this.queue[comparePosition];
-
-                    //switch value with the smallest of the right or left
-                    this.queue[comparePosition] = this.queue[currentPosition];
-                    this.queue[currentPosition] = pointer;
-
-                    //set new compare positions
-                    currentPosition = comparePosition;
-
-                    //if no value in branch, set it to compare priority so while statement will exit. if values equal - no execute
-                    rightBranch = currentPosition * 2 < this.queue.length ? this.queue[currentPosition * 2] : this.queue[currentPosition];
-                    leftBranch = currentPosition * 2 + 1 < this.queue.length ? this.queue[currentPosition * 2 + 1] : this.queue[currentPosition];
-
-                    // console.log(this.queue);
-                }
+            //if parent element has a higher priority (lower value) then break
+            if (element.priority >= parent.priority) {
+                break;
             }
 
-            return toRemove;
+            //else swpap the current element with the parent
+            this.queue[parentPosition] = element;
+            this.queue[position] = parent;
+
+            //update the position of the element that is being sorted
+            position = parentPosition;
+
         }
     }
-    peek() {
-        if (this.queue.length > 1) {
-            return this.queue.slice(1, 1);
+
+    _bubbleDown(position) {
+
+        while (true) {
+
+            let leftChildPos = (position * 2) + 1;
+            let leftChild = this.queue[leftChildPos];
+
+            let rightChildPos = leftChildPos + 1;
+            let rightChild = this.queue[rightChildPos];
+
+            let minIndex = position;
+
+            //if child 1 (left child) exists && has a higher priorty than min element 
+            if (leftChild && (leftChild.priority - this.queue[minIndex].priority < 0)) {
+                //store this position as having a higher priority
+                minIndex = leftChildPos;
+            }
+
+            //if child 2 (right child) exists && has a higher priorty than min element 
+            if (rightChild && (rightChild.priority - this.queue[minIndex].priority < 0)) {
+                minIndex = rightChildPos;
+            }
+
+            //if a higher prioriry element was found
+            if (minIndex !== position) {
+                let pointer = this.queue[minIndex];
+
+                //swap values 
+                this.queue[minIndex] = this.queue[position];
+                this.queue[position] = pointer;
+
+                //update position value 
+                position = minIndex;
+            }
+            else {
+                break;
+            }
         }
+    }
+    
+    peek() {
+        return this.queue[0];
     }
 
     print() {
-        console.log(this.queue);
+        console.log(this.queue.map(function(val) { return val.priority }));
     }
 }
 
 const priorityQueue = new PriorityQueue();
 
-priorityQueue.insert(5); // [5]
-priorityQueue.insert(1); // [1,5]
-priorityQueue.insert(10); // [1,5,10]
-priorityQueue.insert(4); // [1,4,10,5]
-priorityQueue.insert(11); // [1,4,10,11]
-priorityQueue.insert(9); // [1,4,9,5,11,10]
+priorityQueue.enqueue(5); // [5]
+priorityQueue.enqueue(1); // [1,5]
+priorityQueue.enqueue(10); // [1,5,10]
+priorityQueue.enqueue(4); // [1,4,10,5]
+priorityQueue.enqueue(11); // [1,4,10,11]
+priorityQueue.enqueue(9); // [1,4,9,5,11,10]
 
 priorityQueue.print(); // [1,4,9,5,11,10]
 
-priorityQueue.remove(); //1
+priorityQueue.dequeue(); //1
 priorityQueue.print(); // [4,5,9,10,11]
 
-priorityQueue.remove(); //4
+priorityQueue.dequeue(); //4
 priorityQueue.print(); // [5,10,9,11]
 
-priorityQueue.remove(); //5
+priorityQueue.dequeue(); //5
 priorityQueue.print(); // [9,10,11]
 
-priorityQueue.insert(9); // [9,9,11,10]
-priorityQueue.insert(11); // [9,9,11,10,11]
-priorityQueue.insert(10); // [9,9,10,10,11,11]
-priorityQueue.insert(12); // [9,9,10,10,11,11,12]
+priorityQueue.enqueue(9); // [9,9,11,10]
+priorityQueue.enqueue(11); // [9,9,11,10,11]
+priorityQueue.enqueue(10); // [9,9,10,10,11,11]
+priorityQueue.enqueue(12); // [9,9,10,10,11,11,12]
 
 
 priorityQueue.print(); // [9,9,10,10,11,11,12]
 
-priorityQueue.remove(); // [9]
+priorityQueue.dequeue(); // [9]
 priorityQueue.print(); // [9,10,10,12,11,11]
 
-priorityQueue.remove(); // [9]
+priorityQueue.dequeue(); // [9]
 priorityQueue.print(); // [10,11,10,12,11]
 
-priorityQueue.remove(); // [10]
+priorityQueue.dequeue(); // [10]
 priorityQueue.print(); // [10,11,12,11]
 
-priorityQueue.remove(); // [10]
+priorityQueue.dequeue(); // [10]
 priorityQueue.print(); // [11,12,11]
 
-priorityQueue.remove(); // [11]
+priorityQueue.dequeue(); // [11]
 priorityQueue.print(); // [11,12]
 
-priorityQueue.remove(); // [11]
+priorityQueue.dequeue(); // [11]
 priorityQueue.print(); // [12]
 
-priorityQueue.remove(); // [12]
+priorityQueue.dequeue(); // [12]
 priorityQueue.print(); // [null]
 
-priorityQueue.remove(); // undefined
+priorityQueue.dequeue(); // undefined
 priorityQueue.print(); // [null]
