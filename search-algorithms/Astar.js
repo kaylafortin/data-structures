812d13bd2f6961a832aeff5e5a4b532f6e21dd93
hennigk <kayla.hennig@gmail.com>
2018-02-21 15:30:55 +0000

    second version of aStar search

--- search-algorithms/Astar.js
+++ search-algorithms/Astar.js
@@ -31,33 +31,27 @@ class AStar {
 
         this.root = root;
         this.end = end;
-        this.graph = graph.data;
+        this.graph = graph;
+
         let data = graph.data;
 
-        if (end) {
-            data.forEach(function(node, index){
-                let breadthFirst = new BreadthFirst();
-                graph.clear();
-              let path =  breadthFirst.start(data, node.value, end);
-           
-            node.hScore = path.distance;
-        });
-        }
-
+        //if an end has been set - use BFS to assign an hScore
+        //hScore = distance from the node we are searching for
+        //distance is defined by how many nodes away
+        this._setDistances();
+        
+        
+        //create new priority Queue
         let priorityQueue = new PriorityQueue();
-
-
-
-        data[root].priority = 0;
-        //add first node to queue and set prop to visited
-
-
-
+        
+        //TODO not sure about this - does starting node need h or f score? 
+        data[root].priority = data[root].hScore;
+        
+        //add first node to queue and set prop to visited & completed
         data[root].visited = true;
         data[root].completed = true;
 
         priorityQueue.enqueue(data[root]);
-
 
         let path = [];
         //continue until the queue is empty
@@ -67,91 +61,70 @@ class AStar {
             let current = priorityQueue.dequeue();
 
             current.completed = true;
-            console.log('c', current);
-
-            // completed.push(current);
+            
+            //TODO do we need to calculate path like this? prob not
             path.push(current.value);
 
             //if it is the end point - exit loop
             if (current.value == end && end) {
-                console.log('end')
+                console.log('end');
                 break;
             }
-            // console.log(graph, current)
+
             //get the value for the adjacent elements 
             let edges = data[current.value].adjacent;
 
-
             edges.forEach(function(adjacent, index) {
-                // console.log('ad: ', adjacent);
 
                 let neighbor = data[adjacent.value];
-                // console.log('n: ', neighbor)
 
                 //if we have not completed this node to our path
                 if (!neighbor.completed && neighbor) {
-
+                    
+                    //gScore = current weight plus neighbor weight
+                    //fScore = gScore + hScore
                     let gScore = current.gScore + adjacent.weight;
                     let fScore = gScore + neighbor.hScore;
-                    // console.log(adjacent.weight, current.priority, fScore);
-                    
-                    //if already in the queue - updtae the priority
-                    //and if the new score is better than the origin priorority
+
+                    //if already in the queue and 
+                    //if the new score is better than the origin priorority: 
+                    //update the priority
                     if (neighbor.visited && fScore < neighbor.priority) {
+                        //TODO this function prob could use soem work
                         neighbor = priorityQueue.reprioritizeElement(neighbor);
-                        console.log(neighbor)
                     }
-                    if (neighbor){
-                    neighbor.visited = true;
-                    neighbor.gScore = gScore;
-                    neighbor.priority = fScore;
-                    priorityQueue.enqueue(neighbor);
-
-                    console.log('n: ', neighbor)
-}
+                    if (neighbor) {
+                        neighbor.visited = true;
+                        neighbor.gScore = gScore;
+                        neighbor.priority = fScore;
+                        priorityQueue.enqueue(neighbor);
+                        neighbor.predecessor = current.value;
+                    }
                 }
-                //if the adjacent elements haven't been searched add them to queue, 
-                //mark them as visited and indicate their parent
-
-                // if (!graph[adjacent].visited) {
-                //     graph[adjacent].visited = true;
-                //     graph[adjacent].predecessor = current.value;
-
-                //     queue.enqueue(graph[adjacent]);
-                // }
-                // console.log(priorityQueue.data());
             });
-            console.log('p:', path);
-            console.log('q ', priorityQueue.data());
         }
         //store the updated graph as a property
         //and call method to print results
         // console.log(path);
-        this.graph = graph;
+        this.graph = data;
         this.path = path;
-        return this._printResults(this.graph[end]);
+        return this._printResults(this.graph[this.end]);
     }
 
     _setDistances() {
-        const breadthFirst = new BreadthFirst();
+        let breadthFirst = new BreadthFirst();
         let graph = this.graph;
         let end = this.end;
-        // console.log('g', this.graph)
-        graph.forEach(function(node, index){
-            // console.log('d', node.value, end)
-            // let path = [];
-            //  path = breadthFirst.start(graph, node.value, end);
+        let data = this.graph.data;
         
-            new Promise((resolve, reject) => { 
-                let path = breadthFirst.start(graph, node.value, end);
-                 node.hScore  = () => resolve(path.distance);
-                // return node.hScore;
+        if (end) {
+            data.forEach(function(node, index) {
+                graph.clear();
+                let path = breadthFirst.start(data, node.value, end);
+                node.hScore = path.distance;
             });
-            // console.log('search', path)
-            // console.log(node);
-        });
-        console.log('graph', graph)
-        return this.graph = graph
+        }
+        return this.graph.data = data;
     }
 
     _printResults(end) {
@@ -182,14 +155,14 @@ class AStar {
 
 // module.exports.AStar = AStar;
 
-const VERTICES = 4;
+const VERTICES = 7;
 
 let graph = new Graph();
 
 let aStar = new AStar();
 // console.log(aStar.start)
 graph.createWeighted(VERTICES);
-console.log(graph.data);
+// console.log(graph.data);
 
 aStar.start(graph, 0, 3);
 
